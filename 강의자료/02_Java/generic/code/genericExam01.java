16141e42d7abbf19ad7fabedd00e02b6a92e0eb1
김경훈 <kkh3363@gmail.com>
2025-07-04 11:44:24 +0900

    Create genericExam01.java

---
+++ 강의자료/02_Java/generic/code/genericExam01.java
@@ -0,0 +1,10 @@
+public class GenericExam1 {
+
+	public static void main(String[] args) {
+		Cup<Beer> beer= new Cup<Beer>();
+		Cup<Boricha> boricha= new Cup<Boricha>();
+		Cup<Milk> milk= new Cup<Milk>();
+		
+	}
+
+}
