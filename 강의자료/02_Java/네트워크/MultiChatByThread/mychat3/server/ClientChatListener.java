feb3b26e527ff6eb2e1d7c5d08843664fca4edea
김경훈 <kkh3363@gmail.com>
2025-07-31 00:02:21 +0900

    Add files via upload

---
+++ 강의자료/02_Java/네트워크/MultiChatByThread/mychat3/server/ClientChatListener.java
@@ -0,0 +1,27 @@
+package mychat3.server;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientChatListener implements ActionListener{
+	ClientChatView view;
+
+	public ClientChatListener(ClientChatView view) {
+		super();
+		this.view = view;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==view.txtinput ||
+				e.getSource()==view.btnsend){
+			view.sendMsg("chatting/"+
+					view.txtinput.getText().trim()
+					+"/"+view.nickname);
+			view.txtinput.setText("");
+		}
+		
+	}
+	
+	
+}
