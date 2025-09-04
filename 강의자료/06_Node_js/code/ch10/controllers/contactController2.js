5612fcd6e29a011f08e161dcb7127e712432763a
김경훈 <kkh3363@gmail.com>
2025-09-04 23:16:49 +0900

    Create contactController2.js

---
+++ 강의자료/06_Node_js/code/ch10/controllers/contactController2.js
@@ -0,0 +1,8 @@
+const getAllContacts = asyncHandler(async (req, res) => {
+
+  const users = [
+    { name: "John", email: "john@aaa.bbb", phone: "123456789" },
+    { name: "Jane", email: "jane@aaa.bbb", phone: "67891234" },
+  ];
+  res.render(“getAll”, { heading: “User List” });
+});
