337bcbbc637e8193836501c12f13265396b200f6
김경훈 <kkh3363@gmail.com>
2025-09-04 23:39:02 +0900

    Update app1002.js

--- 강의자료/06_Node_js/code/ch10/app1002.js
+++ 강의자료/06_Node_js/code/ch10/app1002.js
@@ -1,1 +1,28 @@
 
+const express = require("express");
+const ejs = require('ejs');
+const app = express();
+
+const port = 3000;
+
+app.set('view engine','ejs');
+app.set('views', './views')
+
+app.use(express.static("./assets"));
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Hello Node!");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/contacts", require("./routes/contactRoutes"));
+
+
+
+app.listen(port, () => {
+  console.log(`${port}번 포트에서 서버 실행 중`);
+});
