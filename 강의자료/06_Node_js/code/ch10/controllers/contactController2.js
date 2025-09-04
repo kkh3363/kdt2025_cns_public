b49b5725879ed48a08378211d89e1edbb7dfcc8e
김경훈 <kkh3363@gmail.com>
2025-09-04 23:40:22 +0900

    Update contactController2.js

--- 강의자료/06_Node_js/code/ch10/controllers/contactController2.js
+++ 강의자료/06_Node_js/code/ch10/controllers/contactController2.js
@@ -4,5 +4,6 @@ const getAllContacts = asyncHandler(async (req, res) => {
     { name: "John", email: "john@aaa.bbb", phone: "123456789" },
     { name: "Jane", email: "jane@aaa.bbb", phone: "67891234" },
   ];
-  res.render(“getAll”, { heading: “User List” });
+  res.render(“getAll”, { heading: “User List” , users});
+  //res.render(“index”, { heading: “User List” , users});
 });
